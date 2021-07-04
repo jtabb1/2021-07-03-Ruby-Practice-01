a0af992c0a09a5826d2871e668a0454df58bcec5
Jeremiah Tabb <55849254+jtabb1@users.noreply.github.com>
2021-07-04 20:45:54 +0000

    Updated with official soln for oxford commas for reference only

--- main.rb
+++ main.rb
@@ -86,7 +86,7 @@ end
 # https://github.com/jtabb1/fnp3-036-iteration-procedural-ruby-phase-3-oxford-comma
 # oxford_comma.rb
 
-def oxford_comma(array)
+def oxford_comma_0(array)
   len = array.size
   returns = array[0]
   case len
@@ -102,6 +102,16 @@ def oxford_comma(array)
     end
     returns = "#{returns}, and #{array[len-1]}"
   end
+end
+
+# official solution
+def oxford_comma(array)
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
+  end
+  array.join(", ")    
 end
 
 puts oxford_comma(['qa','sw','rf','ui'])
