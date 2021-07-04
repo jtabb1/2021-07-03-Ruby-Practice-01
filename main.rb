c34e9e23e6bbf7a352ce5157b0d845efbf9287c3
Jeremiah Tabb <55849254+jtabb1@users.noreply.github.com>
2021-07-04 01:28:21 +0000

    square array lab tests pass

--- main.rb
+++ main.rb
@@ -1,10 +1,22 @@
 # https://github.com/jtabb1/fnp3-034-phase-3-square_array
 
+def square_array_01(array)
+  # your code here
+  return_array = []
+  array.each do |a| 
+    return_array.push(a*a)
+  end
+  return_array
+end
+
 def square_array(array)
   # your code here
   return_array = []
-  array.each{|a| return_array.push(a*a)}
+  array.each{|a| return_array.push(a**2)}
+  return_array
 end
+
+puts(square_array([1,2,3]))
 
 puts(square_array([1,2,3]).inspect)
 
